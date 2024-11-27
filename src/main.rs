5868947efb448acf191fd7f22ffe207046186485
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2024-11-28 00:29:16 +0800

    read and print test pdf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+
+    let bytes = std::fs::read("test.pdf").unwrap();
+    let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
+    println!("{}", out);
+    // assert!(out.contains("This is a small demonstration"));
 }
