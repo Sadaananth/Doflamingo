49b1f1cfc1f1020196d06a5971bb99196efae081
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2024-12-08 16:35:31 +0800

    Pass hardcoded salary from slint to rust

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,30 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use duckdb::{params, Connection, Result};
-use duckdb::arrow::record_batch::RecordBatch;
-use duckdb::arrow::util::pretty::print_batches;
+use duckdb::{Connection, Result};
 use std::error::Error;
-use std::io::{self, Write};
 
 slint::include_modules!();
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
-}
+mod salary;
+use crate::salary::salary::add_salary;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("starting");
-    io::stdout().flush().unwrap();
     let conn = Connection::open_in_memory().expect("Failed to open connection");
 
     conn.execute_batch(
         r"CREATE SEQUENCE seq;
-          CREATE TABLE person (
+          CREATE TABLE salary (
                   id              INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq'),
                   name            TEXT NOT NULL,
-                  data            BLOB
+                  date            TEXT NOT NULL,
+                  salary          INTEGER,
+                  bonus           INTEGER
                   );
          ").expect("Failed to create table");
-    let me = Person {
-        id: 0,
-        name: "Test".to_string(),
-        data: None,
-    };
-    println!("id {}", me.id);
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?, ?)",
-        params![me.name, me.data],
-    ).expect("Failed to execute");
-
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    }).expect("Failed to query_map");
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-
-    // query table by arrow
-    let rbs: Vec<RecordBatch> = stmt.query_arrow([])?.collect();
-    let _ = print_batches(&rbs);
-    println!("All well");
-
     let ui = AppWindow::new()?;
+    ui.on_add_salary(move |date, salary| {
+        add_salary(date.to_string(), salary.try_into().unwrap());
+    });
     ui.run()?;
 
     Ok(())
--- src/salary.rs
+++ src/salary.rs
@@ -1,3 +1,5 @@
-fn add_salary() {
-
+pub mod salary {
+pub fn add_salary(date: String, amount: u32) {
+    println!("{} {}", date, amount);
+}
 }
