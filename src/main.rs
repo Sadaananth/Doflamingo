46d12be76373b5e0a32b90c2a2b7d16194618cde
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2024-12-10 22:09:45 +0800

    more changes

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,16 @@ mod database;
 use database::database::DatabaseHandler;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let today = Local::now();
     let database_handler = DatabaseHandler::new();
     database_handler.init_handler();
-
+    
     let salary_handler = SalaryHandler::new(database_handler);
-
+    
+    let today = Local::now();
     let ui = AppWindow::new()?;
+    ui.global::<Data>().set_this_year(today.year());
+    ui.global::<Data>().set_this_month(today.month().try_into().unwrap());
+    ui.global::<Data>().set_today(today.day().try_into().unwrap());
     ui.on_add_salary(move |date, salary| {
         salary_handler.add_salary(date.to_string(), salary.try_into().unwrap());
     });
