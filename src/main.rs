e0321cdc014938fb4a1a30b9ca3381f10d097b6e
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2024-12-29 02:14:59 +0800

    add overview page

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,77 @@ use chrono::prelude::*;
 use duckdb::Result;
 use std::error::Error;
 use std::rc::Rc;
-use Doflamingo::database;
 
 slint::include_modules!();
+use plotters::prelude::*;
+use slint::SharedPixelBuffer;
 
 use Doflamingo::database::database::DatabaseHandler;
 use Doflamingo::debt::debt::DebtHandler;
 use Doflamingo::salary::salary::SalaryHandler;
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+mod wasm_backend;
+
+fn pdf(x: f64, y: f64, a: f64) -> f64 {
+    const SDX: f64 = 0.1;
+    const SDY: f64 = 0.1;
+    let x = x as f64 / 10.0;
+    let y = y as f64 / 10.0;
+    a * (-x * x / 2.0 / SDX / SDX - y * y / 2.0 / SDY / SDY).exp()
+}
+
+fn render_plot(pitch: f32, yaw: f32, amplitude: f32) -> slint::Image {
+    let mut pixel_buffer = SharedPixelBuffer::new(640, 480);
+    let size = (pixel_buffer.width(), pixel_buffer.height());
+
+    let backend = BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size);
+
+    // Plotters requires TrueType fonts from the file system to draw axis text - we skip that for
+    // WASM for now.
+    #[cfg(target_arch = "wasm32")]
+    let backend = wasm_backend::BackendWithoutText { backend };
+
+    let root = backend.into_drawing_area();
+
+    root.fill(&WHITE).expect("error filling drawing area");
+
+    let mut chart = ChartBuilder::on(&root)
+        .build_cartesian_3d(-3.0..3.0, 0.0..6.0, -3.0..3.0)
+        .expect("error building coordinate system");
+    chart.with_projection(|mut p| {
+        p.pitch = pitch as f64;
+        p.yaw = yaw as f64;
+        p.scale = 0.7;
+        p.into_matrix() // build the projection matrix
+    });
+
+    chart.configure_axes().draw().expect("error drawing");
+
+    chart
+        .draw_series(
+            SurfaceSeries::xoz(
+                (-15..=15).map(|x| x as f64 / 5.0),
+                (-15..=15).map(|x| x as f64 / 5.0),
+                |x, y| pdf(x, y, amplitude as f64),
+            )
+            .style_func(&|&v| {
+                (&HSLColor(240.0 / 360.0 - 240.0 / 360.0 * v / 5.0, 1.0, 0.7)).into()
+            }),
+        )
+        .expect("error drawing series");
+
+    root.present().expect("error presenting");
+    drop(chart);
+    drop(root);
+
+    slint::Image::from_rgb8(pixel_buffer)
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 fn main() -> Result<(), Box<dyn Error>> {
     let database_handler = Rc::new(DatabaseHandler::new(true));
     database_handler.init_handler();
@@ -50,6 +113,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             debt_handler.database_handler.print_debt();
         }
     });
+    ui.on_render_plot(render_plot);
     ui.run()?;
 
     Ok(())
