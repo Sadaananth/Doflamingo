3364c613b2dda0f26c811e7347bae0bb44d62bac
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2025-09-16 23:38:14 +0800

    remove some files

--- configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export SLINT_BACKEND="winit-software"
-
-CMAKE_PREFIX_PATH="~/tools/Slint-cpp-1.13.1-Linux-x86_64"
-
-mkdir -p _build
-cmake -B _build -S . -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH"
